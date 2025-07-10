1aca5004b91a0f4640de476fa3c87aa769ec01d8
Babulohar785 <vivekanands900@gmail.com>
2025-07-10 19:43:17 +0545

    frontend_setup

---
+++ resources/views/frontend/index.blade.php
@@ -0,0 +1,30 @@
+@extends('frontend.layouts.master')
+@section('content')
+   
+    <!-- Navbar End -->
+
+
+    <!-- Header Start -->
+    @include('frontend.partial.hero')
+    <!-- Header End -->
+
+
+    <!-- About Start -->
+    @include('frontend.partial.about')
+    <!-- About End -->
+
+
+    <!-- Feature Start -->
+     @include('frontend.partial.feature')
+    <!-- Feature Start -->
+
+
+    <!-- Courses Start -->
+  @include('frontend.partial.courses')
+    <!-- Courses End -->
+
+
+    <!-- Team Start -->
+    @include('frontend.partial.team')
+    <!-- Team End -->
+@endsection
